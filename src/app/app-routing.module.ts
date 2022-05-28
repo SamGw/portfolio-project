307533e4da4658c3f09ae83fe074c4d3ed2ed730
Quentin Martinez <Q.Marti@outlook.fr>
2022-05-28 15:40:05 +0200

    travail sur footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { PortfolioListComponent } from './pages/portfolio-list/portfolio-list.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 const routes: Routes = [
+  { path: 'Footer', component: FooterComponent},
   { path: 'portfolio-list', component: PortfolioListComponent },
   { path: 'login', component: LoginPageComponent },
   { path: '**', component: LoginPageComponent },
--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedModule } from "../../shared.module";
 
 @Component({
   selector: 'app-footer',
