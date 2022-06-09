7ca0161ce9d5491e00c416f172020a55dc1d63b5
Sam Gwynne <sam.gwynne@epita.fr>
2022-06-09 15:57:18 +0200

    feat: add home page; update nav-menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { PortfolioListComponent } from './pages/portfolio-list/portfolio-list.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { HomeComponent  } from './pages/home/home.component';
 
 const routes: Routes = [
-  { path: 'Footer', component: FooterComponent},
+  { path: 'home', component: HomeComponent },
   { path: 'portfolio-list', component: PortfolioListComponent },
   { path: 'login', component: LoginPageComponent },
+  { path: 'home', component: LoginPageComponent },
   { path: '**', component: LoginPageComponent },
 ];
 
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { PortfolioListComponent } from './portfolio-list/portfolio-list.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     LoginPageComponent,
-    PortfolioListComponent
+    PortfolioListComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule
